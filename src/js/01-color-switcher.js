d1bf902b0ff017186a46aa7dc8c64694caf55760
Ilya <110407302+flpmustafa@users.noreply.github.com>
2022-12-29 16:55:42 +0200

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+  const btnStart = document.querySelector("[data-start]");
+  const btnStop = document.querySelector("[data-stop]");
+  let btnStopDefaultState = btnStop.setAttribute("disabled", "disabled");
+
+
+  let timerId = null;
+
+  btnStart.addEventListener('click', () => {
+    btnStop.removeAttribute("disabled", "disabled");
+    btnStart.setAttribute("disabled", "disabled");
+    
+    timerId = setInterval(() => {
+      let randomColor = getRandomHexColor();
+      document.body.style.backgroundColor = randomColor;
+    }, 1000);
+  });
+  
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  btnStop.addEventListener('click', ()=> {
+    btnStart.removeAttribute("disabled", "disabled")
+    btnStop.setAttribute("disabled", "disabled")
+    clearInterval(timerId);
+  });
 
